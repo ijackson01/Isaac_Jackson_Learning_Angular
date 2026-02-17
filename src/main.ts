beb1b55ffde8e7db09cee3471eac909c888f3f39
Isaac Jackson <w0885848@myscc.ca>
2026-02-17 18:14:14 -0500

    Step 1 - Setup Reactive Forms

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
+import { importProvidersFrom } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app/app.component';
 import { routes } from './app/routes';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(ReactiveFormsModule)
+  ]
 }).catch(err => console.error(err));
